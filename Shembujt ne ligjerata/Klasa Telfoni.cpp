5e4ad6d7e98527cc509fc36cc79e99fa4c2c6739
Kadri Sylejmani <kadri.sylejmani@uni-pr.edu>
2021-02-19 22:50:28 +0100

    File names changes

---
+++ Shembujt ne ligjerata/Klasa Telfoni.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class telefoni{
+private:
+	int gjatesia, gjeresia, trashesia;
+	string prodhuesi;
+public:
+	telefoni() {
+		cout << "Cakto gjeresine, gjatesine dhe trashesine:";
+		cin >> gjatesia >> gjeresia >> trashesia;
+		cout << "Cakto prodhuesin: ";
+		cin >> prodhuesi;
+	}
+	void shtyp_vertikal() {
+		int i = 0;
+		do {
+			cout << prodhuesi[i] << endl;
+			i++;
+		} while (i < prodhuesi.length());
+	}
+};
+void main() {
+	telefoni t1,t2;
+	t1.shtyp_vertikal();
+	t2.shtyp_vertikal();
+}
---
+++ Shembujt ne ligjerata/Llogaritjet me dy numra.cpp
@@ -0,0 +1,38 @@
+//#include<iostream>
+//#include<math.h>
+//#include<iomanip>
+//#include<string>
+//using namespace std;
+//double mes(int a, int b, int c) {
+//	return (a + b + c) / 3.0;
+//}
+//int main() {
+//	int a, b;
+//	cout << "Cakto a dhe b: ";
+//	cin >> a >> b;
+//	int max = (a>b) ? (a) : (b);
+//	int min = (a < b) ? (a) : (b);
+//	if (a - b >= 0)
+//		cout << "Ndryshim pozitiv\n";
+//	else
+//		cout << "Ndryshimi negativ\n";
+//	switch (a)
+//	{
+//	case 1:
+//		cout << "Vlera 1"<<endl;
+//		break;
+//	case 2:
+//	case 3:
+//	case 4:
+//		cout << "Vlera 2, 3 ose 4" << endl;
+//		break;
+//	default:
+//		cout << "Asnjera prej vlerave" << endl;
+//		break;
+//	}
+//	for (int i = min; i <= max; i++)
+//		cout << i << endl;
+//	cout << "Mesatarja: " << mes(a, b, a + b) << endl;
+//
+//	return 0;
+//}
