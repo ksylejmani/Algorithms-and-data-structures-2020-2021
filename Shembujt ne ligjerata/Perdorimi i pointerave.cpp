82aa95f8c13b2cec882915c9edeb0070e22e2b32
Kadri Sylejmani <kadri.sylejmani@uni-pr.edu>
2021-03-03 15:52:50 +0100

    Pointerat - Pjesa 1

---
+++ Shembujt ne ligjerata/Perdorimi i pointerave.cpp
@@ -0,0 +1,10 @@
+//#include<iostream>
+//using namespace std;
+//void main() {
+//	int alfa=5, beta=19;
+//	int* palfa, *pbeta;
+//	palfa = &alfa;// Adresa e variables alfa vendoset te poineri palfa
+//	pbeta = &beta;
+//	cout << "alfa=" << alfa << ", beta=" << beta << endl;
+//	cout << "Adresa e alfa: " << palfa << ", Adresa e beta: " << pbeta << endl;
+//}
